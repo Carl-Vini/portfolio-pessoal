b997af4c307fed633a614d5c8f6855886c97ee06
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-05-02 01:54:37 -0300

    header finalizado

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,22 +1,29 @@
+import { useState } from 'react';
 import './Banner.css';
 import { FaBars } from 'react-icons/fa'
 
 const Banner = (props) => {
 
+
+    const [exibirOpcoes, setExibirOpcoes] = useState(false)
+
     const hideNav = () => {
-        
+        setExibirOpcoes(!exibirOpcoes)
     }
 
     return (
         <header>
             {/* <img src='foto.png' alt='foto do Carlos' /> */}
-            <FaBars onClick={hideNav} fontSize={25} color='#1C86EE' cursor='pointer'/>
-            <nav>
-                <h2>Home</h2>
-                <h2>About Me</h2>
-                <h2>Projetos</h2>
-            </nav>
+            <FaBars className='botao-exibir' onClick={hideNav} fontSize={25}  />
 
+            {exibirOpcoes && (
+                <nav>
+                    <h2>Home</h2>
+                    <h2>About Me</h2>
+                    <h2>Projetos</h2>
+                </nav>
+            )
+            }
 
         </header>
     )
