2add7e659b6a5ddbb19bb94e8d19f3db5166d966
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-05-04 00:18:19 -0300

    adicinando 4 projetos

--- src/componentes/Projetos/Cards/index.js
+++ src/componentes/Projetos/Cards/index.js
@@ -3,14 +3,16 @@ import './Cards.css'
 const Card = (props) => {
     return (
 
-        <a href={props.href} >
-            <section className='card'>
-                <img src={props.src} alt='imagem do Projeto' width={260} height={180} />
-                <h2>{props.nome}</h2>
-                <h3>{props.descricao}</h3>
-            </section>
-        </a>
 
+
+        <section className='card'>
+            <a href={props.href} >
+                <img src={props.src} alt='imagem do Projeto' width={260} height={180} />
+            </a >
+            <h2 >{props.nome}</h2>
+            <h3>{props.descricao}</h3>
+
+        </section>
     )
 }
 
--- src/componentes/Projetos/index.js
+++ src/componentes/Projetos/index.js
@@ -19,18 +19,18 @@ const Projeto = (props) => {
             href: 'https://life-counter-beta.vercel.app'
         },
         {
-            nome: 'Organograma',
-            descricao: 'Website para organizar equipes',
-            id: 1,
-            src: './projeto-1.png',
-            href: 'https://projeto-organograma.vercel.app'
+            nome: 'Landing Page',
+            descricao: 'Landing Page da Loopstudio',
+            id: 3,
+            src: './projeto-3.jpg',
+            href: 'https://landing-page-loopstudio.vercel.app'
         },
         {
-            nome: 'Organograma',
-            descricao: 'Website para organizar equipes',
-            id: 1,
-            src: './projeto-1.png',
-            href: 'https://projeto-organograma.vercel.app'
+            nome: 'Qr-Code',
+            descricao: 'Criação de um Qr-code',
+            id: 4,
+            src: './projeto-4.png',
+            href: 'https://qr-code-chalenge.vercel.app'
         }
     ]
 
